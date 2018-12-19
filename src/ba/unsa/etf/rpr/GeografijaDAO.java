5bdc6db1ed06181304ca172ba721bf682fd70206
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-12-20 00:28:40 +0100

    zavrsena klasa GeografijaDAO

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,9 +1,6 @@
 package ba.unsa.etf.rpr;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class GeografijaDAO {
@@ -11,6 +8,7 @@ public class GeografijaDAO {
     private static GeografijaDAO instance = null;
     private Connection conn;
     private String url = "baza.db";
+    private Statement statement;
 
     private static void initialize() {
         instance = new GeografijaDAO();
@@ -41,32 +39,126 @@ public class GeografijaDAO {
     public ArrayList<Grad> gradovi() {
         ArrayList<Grad> gradovi = new ArrayList<>();
 
+        String upit = "";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
+            while (resultSet.next()){
+                System.out.println(resultSet);
+            }
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
 
         return gradovi;
     }
 
-    public Grad glavniGrad(String austrija) {
+    public Grad glavniGrad(String drzava) {
+
+        String upit = "select g.* from grad g, drzava d where d.naziv=? and d.glavni_grad = g.id";
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            preparedStatement.setString(1, drzava);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            int id = resultSet.getInt("g.id");
+            String naziv = resultSet.getString("g.naziv");
+            int brojStanovnika = resultSet.getInt("g.broj_stanovnika");
+            return new Grad(id, naziv, brojStanovnika);
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
+
         return null;
     }
 
-    public void obrisiDrzavu(String kina) {
+    public void obrisiDrzavu(String drzava) {
+
+        String upit = "DELETE FROM drzava WHERE naziv = '" + drzava + "';";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            preparedStatement.setString(1, drzava);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
 
     }
 
-    public Drzava nadjiDrzavu(String francuska) {
+    public Drzava nadjiDrzavu(String drzava) {
+
+        String upit = "select * from drzava d where d.naziv=?";
+
+        try {
+
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            preparedStatement.setString(1, drzava);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.isClosed())
+                return null;
+
+            int drzavaId = resultSet.getInt("d.id");
+            Integer gradId = resultSet.getInt("d.glavni_grad");
+            Drzava novaDrzava = new Drzava(drzavaId, gradId, null);
+            Grad glavniGrad = glavniGrad(drzava);
+            glavniGrad.setDrzava(novaDrzava);
+            novaDrzava.setGlavniGrad(glavniGrad);
+            return novaDrzava;
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
         return null;
     }
 
     public void dodajGrad(Grad grad) {
+        if(gradovi().contains(grad)) throw new IllegalArgumentException("Grad vec postoji");
+        String upit = "insert into grad values(?, ?, ?, ?);";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            preparedStatement.setInt(1, grad.getId());
+            preparedStatement.setString(2, grad.getNaziv());
+            preparedStatement.setInt(3, grad.getBrojStanovnika());
+            preparedStatement.setNull(4, 0);
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
 
     }
 
-    public void dodajDrzavu(Drzava bih) {
+    public void dodajDrzavu(Drzava drzava) {
+        String upit = "insert into drzava values(?, ?, ?);";
 
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(upit);
+            preparedStatement.setInt(1, drzava.getId());
+            preparedStatement.setString(2, drzava.getNaziv());
+            preparedStatement.setInt(3, drzava.getGlavniGrad().getId());
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
     }
 
-    public void izmijeniGrad(Grad bech) {
-
+    public void izmijeniGrad(Grad grad) {
+        try{
+            String upit1 = "UPDATE grad set broj_stanovnika = ? where id = ?";
+            String upit2 = "UPDATE grad set name = ? where id = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(upit1);
+            preparedStatement.setInt(1, grad.getBrojStanovnika());
+            PreparedStatement preparedStatement2 = conn.prepareStatement(upit2);
+            preparedStatement.setString(2, grad.getNaziv());
+            preparedStatement.executeUpdate();
+            preparedStatement2.executeUpdate();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+        }
     }
 }
