ef84ae204eaf43a3fd2864b86f2431522da94584
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-12-19 23:29:47 +0100

    zapoceta implementacija

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,21 +1,49 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class GeografijaDAO {
 
+    private static GeografijaDAO instance = null;
+    private Connection conn;
+    private String url = "baza.db";
+
+    private static void initialize() {
+        instance = new GeografijaDAO();
+    }
+
+    private GeografijaDAO() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void removeInstance() {
-
+        instance = null;
     }
 
     public static GeografijaDAO getInstance() {
-        return null;
+        if(instance == null) initialize();
+        return instance;
     }
 
 
     public ArrayList<Grad> gradovi() {
-        return null;
+        ArrayList<Grad> gradovi = new ArrayList<>();
+
+
+
+        return gradovi;
     }
 
     public Grad glavniGrad(String austrija) {
